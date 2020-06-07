f17d36570d552e99e7fdfcbd5cd2e761cf0603a4
Kai <43492495+HIENDQ@users.noreply.github.com>
2020-06-07 23:46:15 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
 
 app.get('/', (req, res) => {
     res.send(`OK`);
