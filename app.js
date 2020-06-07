107c635632248a88417779c3a3c681b489848375
HIENDQ <quyhien98@gmail.com>
2020-06-07 23:59:32 +0700

    s

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 
 const app = express();
-const PORT =  5000;
+const POST =  5000;
 
 app.get('/', (req, res) => {
     res.send(`OK`);
 })
+app.listen(POST, function() {
+    console.log('Server listening on port ' + POST);
+  });
