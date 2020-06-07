085c2a16d02c4d41b8de4c08a8d8b97f08332fbf
HIENDQ <quyhien98@gmail.com>
2020-06-07 23:25:36 +0700

    Change something

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
+require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser')
+var passport = require('passport');
+var mongoose = require('mongoose');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-// app.use('/api/auth', authRoutes);
-// app.use('/api/product', productRoutes);
-app.get('/', (req, res) => {
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+
+app.get('/hi', (req, res) => {
     
     res.send('Say Hi');
 })
