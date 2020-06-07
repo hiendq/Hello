21e570e0cb3b705745eb3e68f0f73fbf4d6570bc
Kai <43492495+HIENDQ@users.noreply.github.com>
2020-06-07 23:44:55 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser')
-var passport = require('passport');
-var mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(passport.initialize());
 
 app.get('/', (req, res) => {
     res.send(`OK`);
