b0739f02896b0909077bb6bc4b4ca8654032f00e
Kai <43492495+HIENDQ@users.noreply.github.com>
2020-06-08 00:07:28 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const port = process.env.PORT || 5000
 app.get('/', (req, res) => {
     res.send(`OK`);
 })
+app.get('/api/hi', (req, res) => {
+    res.send(`Say hi`);
+})
 app.listen(port, function() {
     console.log('Server listening on port ' + port);
   });
