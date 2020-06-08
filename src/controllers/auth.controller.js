f803b77c5bb86f19ec118c5c8e826dd0a8bf19ff
HIENDQ <quyhien98@gmail.com>
2020-06-08 18:27:11 +0700

    Change user info

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -12,7 +12,15 @@ module.exports.login = function(req, res) {
     return res.status(201).json('Func login');
 }
 module.exports.login = function(req, res) {
-    res.status(201).json({message: 'Login here'})
-       
+    res.status(201).json({message: 'Login here'})       
 }
-
+module.exports.getInfoById = function (req, res) {
+    const id = req.params.id;
+    res.status(201).json({message: 'Get account Info'})
+}
+module.exports.changeInfoById = function (req, res) {
+    const id=  req.params.id;
+    let userInfo = res.body;
+    user.id = id;
+    res.status(201).json({message: 'Get account Info'})
+}
