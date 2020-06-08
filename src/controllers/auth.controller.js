1cb4b8b4631f409cee9bd1d00064cdee728cebe6
HIENDQ <quyhien98@gmail.com>
2020-06-08 22:37:40 +0700

    Find user by id user

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,7 @@
-var User = require('../models/user.model')
 const shortid = require('shortid');
+
+var User = require('../models/user.model')
+const _userService = require('../services/auth.services') 
 
 module.exports.logUp = function(req, res) {
     req.body.id = shortid.generate()
@@ -11,10 +13,12 @@ module.exports.logUp = function(req, res) {
 module.exports.login = function(req, res) {
     res.status(201).json({message: 'Login here'})       
 }
-module.exports.getInfoById = function (req, res) {
+module.exports.getInfoById = async function (req, res) {
     const id = req.params.id;
-    res.status(201).json({message: 'Get account Info'})
-}
+    let user = await _userService.findUserByID(id)
+    if(user!==null) res.status(201).json({user})
+    else res.status(201).json({message: 'Not found user'})
+} 
 module.exports.changeInfoById = function (req, res) {
     const id=  req.params.id;
     let userInfo = res.body;
--- src/services/auth.services.js
+++ src/services/auth.services.js
@@ -6,9 +6,16 @@ const changePassword = async (id, newPass) => {
 const changeInfo = async (id, name, phone, address) => {
   return await User.findByIdAndUpdate({_id: id}, {name : name, phone: phone, address: address});
 };
+const findUserByID = async (id) =>{
+  try {
+    return await User.findOne({_id: id})
+  } catch (error) {
+    return null;
+  }
+}
 const removeUserById = async(id) => {
   return await User.remove({ _id: id });
-} 
+}
   module.exports = {
     changePassword,
     changeInfo,
