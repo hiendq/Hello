f20d66994b6031da26c28c374292a291101e4b2d
Kai <43492495+HIENDQ@users.noreply.github.com>
2020-06-09 09:34:29 +0700

    Update auth.controller.js

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -6,5 +6,8 @@ module.exports.logUp = function(req, res) {
     console.log(req.body)
     User.create(req.body)
         .then(res.status(201).json({message: 'Done'}))
-       
+}
+module.exports.login = function(req, res) {
+    const { email, password} = req.body;
+    return res.status(201).json('Func login');
 }
