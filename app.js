6efe187187cae9ad97c1bcbd3d2e55e14c33b72c
HIENDQ <quyhien98@gmail.com>
2020-06-12 09:20:32 +0700

    create api get address

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ const fetch = require('node-fetch');
 var passport = require('passport');
 
 var authRoutes = require('./src/routes/auth.route');
+const City = require('./src/models/City.model')
 const District = require('./src/models/District.model')
 const Ward = require('./src/models/Ward.model');
-const City = require('./src/models/City.model')
+
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -37,17 +38,17 @@ app.get('/', async (req, res) => {
     return res.status(200).json('Done')
 })
 
-app.get('/:id', async (req, res) => {
-    const id = req.params.id;
-    await fetch(`https://thongtindoanhnghiep.co/api/district/${id}/ward`, { method: 'GET'})
-    .then(res => res.json())
-    .then(json => json.map((item, index)=> {
-        const ward = new Ward({city: 3 ,district: 1, code: index, title: item.Title});
-        ward.save();
-        // console.log(ward)
-    }));
-    return res.status(200).json('Done')
-})
+// app.get('/:id', async (req, res) => {
+//     const id = req.params.id;
+//     await fetch(`https://thongtindoanhnghiep.co/api/district/${id}/ward`, { method: 'GET'})
+//     .then(res => res.json())
+//     .then(json => json.map((item, index)=> {
+//         const ward = new Ward({city: 3 ,district: 1, code: index, title: item.Title});
+//         ward.save();
+//         // console.log(ward)
+//     }));
+//     return res.status(200).json('Done')
+// })
 app.get('/api/cities', async (req, res) => {
     City.find()
     .then(result => res.status(201).json(result))
@@ -59,6 +60,14 @@ app.get('/api/city/:id', async (req, res) => {
     District.find({city: id})
     .then(result => res.status(201).json(result))
 })  
+app.get('/api/address/:idCity/:idDistrict/:idWard', async (req, res) => {
+    let address = []
+    const { idCity, idDistrict, idWard }= req.params;
+    await City.find({code: idCity}).then(result => address = [...address,{city: result[0].title}])
+    await District.find({code: idDistrict}).then(result => address = [...address,{district: result[0].title}])
+    await Ward.find({code: idWard}).then(result => address = [...address,{ward: result[0].title}])
+    return res.status(201).json(address);
+}) 
 
 app.get('/api/city/:idCity/district/:idDistrict', async (req, res) => {
     const { idCity, idDistrict}= req.params;
